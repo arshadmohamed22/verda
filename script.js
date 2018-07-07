2a38d2685192287982b3b231b839433c57040239
arshadmohamed22 <38231213+arshadmohamed22@users.noreply.github.com>
2018-07-07 19:34:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 	var popup = document.getElementById('Popup');
 	var btn = document.getElementById("partner-btn");
+	var feedbackBtn = document.getElementById("feedback-btn");
 	var consumerBtn = document.getElementById("consumer-btn");
 	var retailBtn = document.getElementById("retail-btn");
 	var wholesaleBtn = document.getElementById("wholesale-btn");
 	var span = document.getElementsByClassName("close")[0];
 
 	btn.onclick = function() {
+	    popup.style.display = "block";
+	}
+	feedbackBtn.onclick = function() {
 	    popup.style.display = "block";
 	}
 	consumerBtn.onclick = function() {
@@ -20,6 +24,18 @@ $(document).ready(function() {
 	}
 	
 	span.onclick = function() {
+		$('#btn-group-3').addClass("active");
+		$('#signup-title').addClass("active");
+		$('#feedback-title').removeClass("active");
+
+	    $('#consumer-sign-up-btn').addClass("active-btn");
+	    $('#retail-sign-up-btn').removeClass("active-btn");
+	    $('#wholesale-sign-up-btn').removeClass("active-btn");
+
+	    $('#consumer-sign-up').addClass("active-form");
+	    $('#retail-sign-up').removeClass("active-form");
+	    $('#wholesale-sign-up').removeClass("active-form");
+	    $('#feedback').removeClass("active-form");
 	    popup.style.display = "none";
 	}
 
@@ -30,10 +46,23 @@ $(document).ready(function() {
 	}
 
 
+	$("#feedback-btn").click(function() {
+			$('#btn-group-3').removeClass("active");
+			$('#signup-title').removeClass("active");
+			$('#feedback-title').addClass("active");
 
+		    $('#consumer-sign-up').removeClass("active-form");
+		    $('#retail-sign-up').removeClass("active-form");
+		    $('#wholesale-sign-up').removeClass("active-form");
+		    $('#feedback').addClass("active-form");
+	});
 
 	// consumer form
 	$("#consumer-sign-up-btn").click(function() {
+		$('#btn-group-3').addClass("active");
+		$('#signup-title').addClass("active");
+		$('#feedback-title').removeClass("active");
+
 	    $('#consumer-sign-up-btn').addClass("active-btn");
 	    $('#retail-sign-up-btn').removeClass("active-btn");
 	    $('#wholesale-sign-up-btn').removeClass("active-btn");
@@ -41,9 +70,14 @@ $(document).ready(function() {
 	    $('#consumer-sign-up').addClass("active-form");
 	    $('#retail-sign-up').removeClass("active-form");
 	    $('#wholesale-sign-up').removeClass("active-form");
+	    $('#feedback').removeClass("active-form");
 	});
 		//bottom of consumer page button
 		$("#consumer-btn").click(function() {
+			$('#btn-group-3').addClass("active");
+			$('#signup-title').addClass("active");
+			$('#feedback-title').removeClass("active");
+
 			$('#consumer-sign-up-btn').addClass("active-btn");
 		    $('#retail-sign-up-btn').removeClass("active-btn");
 		    $('#wholesale-sign-up-btn').removeClass("active-btn");
@@ -51,10 +85,15 @@ $(document).ready(function() {
 		    $('#consumer-sign-up').addClass("active-form");
 		    $('#retail-sign-up').removeClass("active-form");
 		    $('#wholesale-sign-up').removeClass("active-form");
+		    $('#feedback').removeClass("active-form");
 		});
 
 	//retail form
 	$('#retail-sign-up-btn').click(function() {
+		$('#btn-group-3').addClass("active");
+		$('#signup-title').addClass("active");
+		$('#feedback-title').removeClass("active");
+
 	    $('#consumer-sign-up-btn').removeClass("active-btn");
 	    $('#retail-sign-up-btn').addClass("active-btn");
 	    $('#wholesale-sign-up-btn').removeClass("active-btn");
@@ -62,9 +101,14 @@ $(document).ready(function() {
 	    $('#consumer-sign-up').removeClass("active-form");
 	    $('#retail-sign-up').addClass("active-form");
 	    $('#wholesale-sign-up').removeClass("active-form");
+	    $('#feedback').removeClass("active-form");
 	});
 		//bottom of retail page button
 		$('#retail-btn').click(function() {
+			$('#btn-group-3').addClass("active");
+			$('#signup-title').addClass("active");
+			$('#feedback-title').removeClass("active");
+
 		    $('#consumer-sign-up-btn').removeClass("active-btn");
 		    $('#retail-sign-up-btn').addClass("active-btn");
 		    $('#wholesale-sign-up-btn').removeClass("active-btn");
@@ -72,10 +116,15 @@ $(document).ready(function() {
 		    $('#consumer-sign-up').removeClass("active-form");
 		    $('#retail-sign-up').addClass("active-form");
 		    $('#wholesale-sign-up').removeClass("active-form");
+		    $('#feedback').removeClass("active-form");
 		});
 
 	//wholesale form
 	$('#wholesale-sign-up-btn').click(function() {
+		$('#btn-group-3').addClass("active");
+		$('#signup-title').addClass("active");
+		$('#feedback-title').removeClass("active");
+
 	    $('#consumer-sign-up-btn').removeClass("active-btn");
 	    $('#retail-sign-up-btn').removeClass("active-btn");
 	    $('#wholesale-sign-up-btn').addClass("active-btn");
@@ -83,9 +132,14 @@ $(document).ready(function() {
 	    $('#consumer-sign-up').removeClass("active-form");
 	    $('#retail-sign-up').removeClass("active-form");
 	    $('#wholesale-sign-up').addClass("active-form");
+	    $('#feedback').removeClass("active-form");
 	});
 		//bottom of wholesale page button
 		$('#wholesale-btn').click(function() {
+			$('#btn-group-3').addClass("active");
+			$('#signup-title').addClass("active");
+			$('#feedback-title').removeClass("active");
+
 		    $('#consumer-sign-up-btn').removeClass("active-btn");
 		    $('#retail-sign-up-btn').removeClass("active-btn");
 		    $('#wholesale-sign-up-btn').addClass("active-btn");
@@ -93,5 +147,6 @@ $(document).ready(function() {
 		    $('#consumer-sign-up').removeClass("active-form");
 		    $('#retail-sign-up').removeClass("active-form");
 		    $('#wholesale-sign-up').addClass("active-form");
+		    $('#feedback').removeClass("active-form");
 		});
 });
